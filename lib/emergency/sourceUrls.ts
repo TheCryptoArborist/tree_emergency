5c58a4183bc2076e00698fcfba37e16465ebd623
TheCryptoArborist <ptshopping63@gmail.com>
2026-07-20 15:50:34 -0500

    Add official emergency source registry

---
+++ lib/emergency/sourceUrls.ts
@@ -0,0 +1,13 @@
+export const sourceUrls = {
+  mobile311Public: "https://www.cityofmobile.org/311/",
+  alabamaPowerOutageMap: "https://outagemap.alabamapower.com/",
+  nwsMobilePensacola: "https://www.weather.gov/mob/",
+  nwsApiPoint: "https://api.weather.gov/points/30.6954,-88.0399",
+  nhcHome: "https://www.nhc.noaa.gov/",
+  nhcAtlanticOutlook: "https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=7",
+  nhcAtlanticTwoDayOutlook: "https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=2",
+  nhcAtlanticSevenDayImage: "https://www.nhc.noaa.gov/xgtwo/two_atl_7d0.png",
+  nhcActiveCyclones: "https://www.nhc.noaa.gov/cyclones/",
+  nhcGis: "https://www.nhc.noaa.gov/gis/",
+  nhcPreparedness: "https://www.noaa.gov/hurricane-prep"
+} as const;
